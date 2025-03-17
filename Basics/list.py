cdd2c41434c10232efe1750c365ee4296a45c410
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2025-03-17 12:05:25 -0600

    Data Types

---
+++ Basics/list.py
@@ -0,0 +1,19 @@
+cars = ["bmw", "honda", "audi"]
+empty_list = []
+print(cars)
+print(empty_list)
+
+x = "test"
+
+print("#"*20)
+print(cars[0])
+
+num_list = [1, 2, 3]
+sum_num = num_list[0] + num_list[1] + num_list[2]
+print(sum_num)
+
+more_cars = ["bmw", "honda", "audi"]
+print(more_cars[0])
+
+more_cars[1] = "Benz"
+print(more_cars)
