87e2560cc10704c8fddf7001cddf6c81b9b33e9d
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-05-20 21:44:02 -0600

    Boolean Operators

---
+++ Basics/boolean_operators.py
@@ -0,0 +1,34 @@
+# And
+print("And")
+# True and True -> True
+print(True and True)
+# True and False -> False
+print(True and False)
+# False and False -> False
+print(False and False)
+
+# Or
+print("\nOr")
+# True or True -> True
+print(True or True)
+# True or False -> True
+print(True or False)
+# False or False -> False
+print(False or False)
+
+# Not
+print("\nNot")
+# not True -> False
+print(not True)
+# not False -> True
+print(not False)
+
+print("\nComparators with boolean operators:")
+and_example = (10 > 5) and (5 > 3)
+print(and_example)
+
+or_example = (10 > 5) or (5 < 3)
+print(or_example)
+
+not_example = not (10 > 5)
+print(not_example)
