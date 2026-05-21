899974968b3e6e00e7a0ff9078426b913a5195ce
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-05-20 21:39:05 -0600

    comparators

---
+++ Basics/comparators.py
@@ -0,0 +1,18 @@
+bool_one = 10 == 10
+print(bool_one)
+
+greater_than = 10 > 5
+print(greater_than)
+
+less_than = 10 < 5
+print(less_than)
+
+not_equal = 10 != 5
+print(not_equal)
+
+greater_equal = 10 >= 10
+print(greater_equal)
+
+less_equal = 10 <= 5
+print(less_equal)   
+
