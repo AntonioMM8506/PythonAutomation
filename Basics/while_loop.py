d6f1c27ec4bd40fcd04215b97ba90933edb80ef6
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-05-26 21:27:50 -0600

    Break and Continue

--- Basics/while_loop.py
+++ Basics/while_loop.py
@@ -1,7 +1,25 @@
 x = 0
 while x < 10:
-    print(x)
     x += 1
+    if x == 8:
+        print("x is 8, breaking the loop")
+        break
+    print(f"value of x: {x}")
+    print("-"*20)
+else:
+    print("Loop completed without break")
+
+
+y = 0
+while y < 10:
+    y += 1
+    if y == 8:
+        print("y is 8, continuing the loop")
+        continue
+    print(f"value of y: {y}")
+    print("-"*20)
+else:
+    print("Loop completed without continue")
 
 
 l = []
