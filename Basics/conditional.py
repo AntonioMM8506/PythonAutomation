7421d5487cb868b2c4d08b0938028f43f0bff38b
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-05-26 21:13:20 -0600

    Conditional

---
+++ Basics/conditional.py
@@ -0,0 +1,15 @@
+if 100 > 10:
+    print("100 is greater than 10")
+if 100 < 10:
+    print("100 is less than 10")
+if 100 == 10:
+    print("100 is equal to 10")
+
+
+value = "green"
+if value == "green":
+    print("GO")
+elif value == "yellow":
+    print("SLOW DOWN")
+else:
+    print("STOP")
