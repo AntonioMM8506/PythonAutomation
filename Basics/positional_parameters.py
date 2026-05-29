86c1491b5720717c65a8fff9caae90c5df9f5f0a
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-05-28 19:47:45 -0600

    Positional Parameters

---
+++ Basics/positional_parameters.py
@@ -0,0 +1,5 @@
+def subtract_numbers(a, b = 1):
+    return a - b
+
+print(subtract_numbers(10))      # Output: 9 (b defaults to 1)
+print(subtract_numbers(10, 5))   # Output: 5 (b is explicitly set to 5)
