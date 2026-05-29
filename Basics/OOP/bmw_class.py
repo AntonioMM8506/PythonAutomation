a4511799dc08f31f807256eb548764448d1faf3d
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-05-28 20:53:22 -0600

    Inheritance

---
+++ Basics/OOP/bmw_class.py
@@ -0,0 +1,18 @@
+import car_class
+
+# Inheritance: BMW is a subclass of Car, inheriting its properties and methods
+# super() is used to call the __init__ method of the parent class (Car) to initialize the make, model, and year attributes.
+# override the drive and stop methods to provide specific behavior for BMW cars.
+class BMW(car_class.Car):
+    def __init__(self, model, year):
+        super().__init__("BMW", model, year)
+
+    def drive(self):
+        print(f"The BMW {self.model} is driving smoothly.")
+
+    def stop(self):
+        print(f"The BMW {self.model} has stopped gracefully.")
+
+print("\nBMW Car:")
+b1 = BMW("X5", 2020)
+b1.display_info()  # Output: 2020 BMW X5
--- Basics/OOP/car_class.py
+++ Basics/OOP/car_class.py
@@ -1,4 +1,6 @@
 class Car(object):
+    wheels = 4
+
     # init method is a special method in Python classes, it is called when an instance of the class is created.
     # self is a reference to the current instance of the class, it is used to access variables that belong to the class.
     def __init__(self, make, model, year):
@@ -9,14 +11,23 @@ class Car(object):
     def display_info(self):
         print(f"{self.year} {self.make} {self.model}")
 
+    def drive(self):
+        print(f"The {self.make} {self.model} is driving.")
+    
+    def stop(self):
+        print(f"The {self.make} {self.model} has stopped.")
+        
+
 c1 = Car("BMW", "X5", 2020)
 c2 = Car("Audi", "Q7", 2021)
 
 print("Car 1:")
 c1.display_info()  # Output: 2020 BMW X5
+print(c1.wheels)  # Output: 4
 print("Car 2:")
 c2.display_info()  # Output: 2021 Audi Q7
 
 c1.year = 2022
 print("\nUpdated Car 1:")
 c1.display_info()  # Output: 2022 BMW X5
+
