b586598191afd01cc10cd69442564a106707d76d
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-07-05 14:44:09 -0600

    Wait Types

---
+++ SeleniumWDTutorial/Advanced Interactions/explicit_and_implicit_wait.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Explicit Wait: Waits for a specific condition to occur before proceeding further in the code.
+# Implicit Wait: Sets a default wait time for the entire duration of the WebDriver session
