8ef4b347cdade9df30c78cf27a31c33770d131ef
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-07-27 20:35:44 -0600

    Logging Format

--- SeleniumWDTutorial/3_Logging/introduction.py
+++ SeleniumWDTutorial/3_Logging/introduction.py
@@ -17,7 +17,10 @@ import os
 
 # Set the logging level to DEBUG to print all messages
 log_file_path = os.path.join(os.path.dirname(__file__), "introduction.log")
-logging.basicConfig(level=logging.DEBUG, filename=log_file_path, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Format of the log messages: timestamp, log level, and message
+logging.basicConfig(level=logging.DEBUG, filename=log_file_path, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%m-%Y %I:%M:%S %p')
+# For datetime formatting, refer to https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
 
 # Will not print DEBUG nor INFO messages, only WARNING, ERROR and CRITICAL, unless the logging level is set to DEBUG or INFO
 logging.info("This is an info message")
