113e5305ca56e090dd7a134ef9ffad121a5068c9
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-07-29 20:58:36 -0600

    class methods in Unit Tests

--- SeleniumWDTutorial/4_UnitTest/test_case_demo.py
+++ SeleniumWDTutorial/4_UnitTest/test_case_demo.py
@@ -2,11 +2,27 @@ import unittest
 
 class TestCaseDemo(unittest.TestCase):
 
+    # decorator @classmethod is used to define a class method that is called before 
+    # any tests in the class are run. 
+    @classmethod
+    def setUpClass(cls):
+        print("#" * 30)
+        print("Setting up TestCaseDemo class...")
+        print("#" * 30)
+
     def test_addition(self):
         self.assertEqual(1 + 1, 2)
 
     def test_subtraction(self):
         self.assertEqual(5 - 3, 2)
 
+    @classmethod
+    def tearDownClass(cls):
+        print("#" * 30)
+        print("Tearing down TestCaseDemo class...")
+        print("#" * 30)
+
 if __name__ == '__main__':
-    unittest.main()
+    # verbosity = 2 provides more detailed test output, including the name of 
+    # each test and its result.
+    unittest.main(verbosity=2)
