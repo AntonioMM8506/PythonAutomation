8a1ec43edb99a2757cbeb3dca2bf8d202488a27f
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-08-03 19:57:39 -0600

    Fixtures

--- PytestFW/test_case_demo1.py
+++ PytestFW/test_case_demo1.py
@@ -1,9 +1,13 @@
 import pytest
 
 # decorator to indicate that the method works as a fixture, so it can later be injected
-@pytest.fixture
+@pytest.fixture()
 def setUp():
     print("Before Each Test")
+    # yield is used to return control to the test method, and after the test method is 
+    # executed, the code after yield will be executed
+    yield
+    print("After Each Test")
 
 def test_methodA(setUp):
     print("Executing test_methodA")
@@ -13,3 +17,7 @@ def test_methodB(setUp):
 
 # To run the tests in this file, you can use the following command in your terminal:
 # pytest -v -s PytestFW/test_case_demo1.py
+
+# To run a particular TC
+# pytest -v -s PytestFW/test_case_demo1.py::test_methodA
+# pytest -v -s PytestFW/test_case_demo1.py -k test_methodB
