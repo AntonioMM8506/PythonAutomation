ee4173c00eacd3c257a80fa6b123aa5c9cfaf7bf
Antonio Maldonado <antonio.manilla.maldonado@gmail.com>
2026-08-20 21:03:32 -0600

    Data Driven Testing

---
+++ AutomationFramework/tests/courses_test_multiple_data.py
@@ -0,0 +1,33 @@
+import unittest
+import pytest
+from AutomationFramework.pages.courses_page import CoursesPage
+from AutomationFramework.pages.login_page import LoginPage
+from ddt import ddt, data, unpack
+
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
+@ddt # This decorator is used to indicate that the test class will use data-driven testing.
+class CoursesTests(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver, self.baseURL)
+        self.cp = CoursesPage(self.driver, self.baseURL)
+
+    @pytest.mark.run(order=1)
+    # This decorator is used to specify the order in which the test methods should be executed.
+    @data(("JavaScript", "4111111111111111", "12/30", "123"), ("Rest API", "4222222222222222", "11/29", "456"))
+    @unpack
+    def test_invalidEnrollment(self, courseName, ccNumber, ccExp, ccCVV):
+        self.lp.open(self.lp.url)
+        self.lp.login("test@email.com", "abcabc")
+        assert self.lp.verifyLoginSuccesful()
+        
+        self.cp.open(self.cp.url)
+        self.cp.searchCourse(courseName)
+        assert self.cp.selectCourse(courseName)
+        self.cp.enroll()
+        self.cp.enterCardNum(ccNumber)
+        self.cp.enterCardExp(ccExp)
+        self.cp.enterCardCVV(ccCVV)
+        self.cp.buy()
+        assert self.cp.verifyEnrollMessageDisplayed()
